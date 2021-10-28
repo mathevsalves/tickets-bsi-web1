532d00de6f47a1edc5d1bfd1e27c6ca5a00dcdd0
Daniel da Silva de Lima <dsl15021996@gmail.com>
2021-10-28 01:23:24 -0300

    - Criado tela de register

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -11,7 +16,13 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class RegisterModule { }
--- src/app/pages/register/register/register.component.ts
+++ src/app/pages/register/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public name: FormControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  public email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  public password: FormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
+
+  public hide: boolean = true;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  public register() {
+    if (!this.validatedForm()) {
+      console.log(
+        this.name.value,
+        this.email.value,
+        this.password.value,
+      );
+      this.router.navigate(['login']);
+    } else
+      alert('dados invalidos');
+  }
+
+  public validatedForm(): boolean {
+    return this.name.invalid || this.email.invalid || this.password.invalid;
+  }
+
+  public login() {
+    alert('vou para login');
+    this.router.navigate(['login']);
+  }
 }
